1a293da7c15ccbfa4393b5e2e12599d424b568dc
Miguelo0098 <i62rarum@uco.es>
2018-10-01 16:14:49 +0200

    fixed list issue

--- chechusounds.py
+++ chechusounds.py
@@ -1,6 +1,6 @@
 import random
 
 def pick_random_voice():
-    voices = ['voices/meperdonas.mp3', 'joder.mp3']
+    voices = ['voices/meperdonas.mp3', 'voices/joder.mp3']
     voice = random.choice(voices)
     return voice
