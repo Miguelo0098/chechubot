05ac7a3d687fee554c635ea06a494295360ffc0d
Miguelo0098 <i62rarum@uco.es>
2018-10-01 09:05:16 +0200

    Añadido archivo principal

---
+++ chechubot.py
@@ -0,0 +1,32 @@
+import discord
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix='<', description='bot stuff')
+
+'''comment text'''
+
+@bot.event
+async def on_ready():
+	print('Logged in as')
+	print(bot.user.name)
+
+@bot.command(pass_context=True)
+async def hola(ctx):
+	await bot.say('@{} Saludos, individuo humanoide.'.format(str(ctx.message.author)))
+
+@bot.command(pass_context=True)
+async def ping(ctx):
+	await bot.say('@{} Pong.'.format(str(ctx.message.author)))
+
+@bot.group(pass_context=True)
+async def role(ctx):
+	if ctx.invoked_subcommand is None:
+		await bot.say('Dame argumentos, coño.')
+		await bot.say('`create -- > crea un rol.\ndelete -- > elimina un rol`')
+		pass
+
+@bot.command(pass_context=True)
+async def moja(ctx):
+	await bot.say('moja')
+
+bot.run('MzYyOTQ1NDE0MzIxNjY4MDk2.DYAFIQ._wULz77Vrzu1kxdmA47SwaWK58k')
